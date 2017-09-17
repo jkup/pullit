1342b9b324680ea7b6e806845edce117b260c3a6
Jon Kuperman <jon.kuperman@gmail.com>
2017-09-16 18:50:13 -0700

    got it working!

--- src/githubApiWrapper.js
+++ src/githubApiWrapper.js
@@ -1,3 +1,16 @@
 const GitHubApi = require('github');
 
-const github = new GitHubApi({});
+const github = new GitHubApi({
+  debug: true
+});
+
+github.pullRequests.getAll(
+  {
+    owner: 'facebook',
+    repo: 'react'
+  },
+  (err, res) => {
+    console.log(err);
+    console.log(res);
+  }
+);
