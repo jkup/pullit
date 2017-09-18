2c36503f2c0f223e3afd585e9778b7ec5516b729
Jon Kuperman <jon.kuperman@gmail.com>
2017-09-17 21:45:34 -0700

    switch to terminal menu

--- src/github.js
+++ src/github.js
@@ -1,5 +1,5 @@
-const Table = require('cli-table2');
 const GitHubApi = require('github');
+const Menu = require('terminal-menu');
 const { execSync } = require('child_process');
 const parse = require('parse-github-repo-url');
 
@@ -44,18 +44,29 @@ class GitHub {
   }
 
   display() {
-    const getResults = this.fetchRequests();
+    this.fetchRequests().then(results => {
+      const menu = Menu({ width: 100, x: 4, y: 2 });
+      menu.reset();
+      menu.write('Currently open pull requests:\n');
+      menu.write('-------------------------\n');
 
-    const table = new Table({
-      head: ['ID', 'Title']
-    });
-
-    getResults.then(results => {
       results.data.forEach(element => {
-        table.push([element.number, element.title]);
+        menu.add(`${element.number} - ${element.title}`);
       });
 
-      console.log(table.toString());
+      menu.add(`Exit`);
+
+      menu.on('select', label => {
+        menu.close();
+        this.fetch(label.split(' ')[0]);
+      });
+      process.stdin.pipe(menu.createStream()).pipe(process.stdout);
+
+      process.stdin.setRawMode(true);
+      menu.on('close', () => {
+        process.stdin.setRawMode(false);
+        process.stdin.end();
+      });
     });
   }
 }
