b4781659255191941e2b9d1977a420f468ebdc61
Karan Thakkar <karanjthakkar@gmail.com>
2017-10-10 14:17:33 +0530

    Accept a specific PR number via CLI. Fixes #8

--- bin/pullit.js
+++ bin/pullit.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 const Pullit = require('../src/index');
 const pullit = new Pullit();
-pullit.display();
+const prId = parseInt(process.argv[2], 10);
+
+if (!isNaN(prId)) {
+  pullit.fetch(prId);
+} else {
+  pullit.display();
+}
